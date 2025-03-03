ae81fe9b8e4f6be131ee4bdca99b3d94cdfd5f0f
Samira M <smuhammedyarova@gmail.com>
2025-03-02 21:08:12 -0500

    Fix IDS03-J

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,13 +1,19 @@
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class LoginLogger {
     private static final Logger logger = Logger.getLogger(LoginLogger.class.getName());
 
     public void logLogin(boolean loginSuccessful, String username) {
+        String sanitizedUsername = sanitizeUser(username);
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizedUsername);
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizedUsername);
         }
+    }
+
+    private String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9]+", username) ? username : "unauthorized user";
     }
 }
