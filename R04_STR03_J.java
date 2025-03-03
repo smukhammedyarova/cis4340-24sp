3046f083132d8364133e67c266128966def2c6e3
smukhammedyarova <160754440+smukhammedyarova@users.noreply.github.com>
2025-03-02 23:11:08 -0500

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -3,9 +3,9 @@ import java.math.BigInteger;
 public class R04_STR03_J {
     public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);  // Issue: Incorrect encoding of noncharacter data
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
+        String s = x.toString();  // Correctly stores numeric data as a valid string
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);  // Maintains data integrity
     }
 }
